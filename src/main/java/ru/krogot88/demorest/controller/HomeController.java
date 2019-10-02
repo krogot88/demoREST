c3f27644a1d2aef857f3d09091cca66f9150ae81
krogot88 <krogot88@yandex.ru>
2019-10-02 17:58:51 +0400

    add list.html with all words in database

--- src/main/java/ru/krogot88/demorest/controller/HomeController.java
+++ src/main/java/ru/krogot88/demorest/controller/HomeController.java
@@ -1,11 +1,23 @@
 package ru.krogot88.demorest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import ru.krogot88.demorest.dao.WordRepository;
+import ru.krogot88.demorest.model.Word;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private WordRepository wordRepository;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getIndexPage() {
@@ -15,5 +27,12 @@ public class HomeController {
     @RequestMapping(value = "/play", method = RequestMethod.GET)
     public String getPlayPage() {
         return "play";
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String getListPage(Model model) {
+        Iterable<Word> iterable = wordRepository.findAll();
+        model.addAttribute("wordList",iterable);
+        return "list";
     }
 }
